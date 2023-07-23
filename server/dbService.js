6a42579dce65986561c5a49c561c2d748e0d8f7c
Abu Bakar Siddique <111344917+siddique-swe@users.noreply.github.com>
2023-07-24 05:15:49 +0600

    Add files via upload

---
+++ server/dbService.js
@@ -0,0 +1,122 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+let instance = null;
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'siddique',
+    password: 'siddique',
+    database: 'web_app',
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.log(err.message);
+    }
+     console.log('db ' + connection.state);
+});
+
+
+class DbService {
+    static getDbServiceInstance() {
+        return instance ? instance : new DbService();
+    }
+
+    async getAllData() {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query = "SELECT * FROM names;";
+
+                connection.query(query, (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    console.log(results);
+                    resolve(results);
+                })
+            });
+            // console.log(response);
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+    async insertNewName(name) {
+        try {
+            const dateAdded = new Date();
+            const id = Date.now().toString();
+            const insertId = await new Promise((resolve, reject) => {
+                const query = "INSERT INTO names (name, date_added, id) VALUES (?,?,?);";
+
+                connection.query(query, [name, dateAdded, id] , (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.insertId);
+                })
+            });
+            return {
+                id : id,
+                name : name,
+                dateAdded : dateAdded
+            };
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async deleteRowById(id) {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query = "DELETE FROM names WHERE id = ?";
+                console.log(query + id);
+    
+                connection.query(query, [id] , (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.affectedRows);
+                })
+            });
+    
+            return response === 1 ? true : false;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async updateNameById(id, name) {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query = "UPDATE names SET name = ? WHERE id = ?";
+    
+                connection.query(query, [name, id] , (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.affectedRows);
+                })
+            });
+    
+            return response === 1 ? true : false;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async searchByName(name) {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query = "SELECT * FROM names WHERE name = ?;";
+
+                connection.query(query, [name], (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results);
+                })
+            });
+
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+module.exports = DbService;
